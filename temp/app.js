da497fad3c09b3a4469ac06f2c19ee09c1b319f9
Hoon <jsh1400512@naver.com>
2019-05-13 20:50:23 +0900

    Create Project

--- temp/app.js
+++ temp/app.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
 app.get('/', function(req, res) {
     res.send('Hello Home Page');
 });
+app.get('/dynamic', function(req, res) {
+    var lis = '';
+    for(var i=0; i<5; i++) {
+        lis = lis + '<li>coding</li>';
+    }
+    var output = `<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <Title>
+    
+            </Title>
+        </head>
+        <body>
+            Hello Dynamic!
+            ${lis}
+        </body>
+    </html>`
+    res.send(output);
+})
 app.get('/login', function(req, res) {
     res.send('Login Please')
-})
+});
 app.listen(3000, function() {
     console.log('Connected 3000 port!');
 });
