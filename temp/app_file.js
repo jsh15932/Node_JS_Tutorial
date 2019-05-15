81491bbde8d07c98e45cf20f1afe64de46d138b5
Hoon <jsh1400512@naver.com>
2019-05-16 04:27:35 +0900

    Create Project

--- temp/app_file.js
+++ temp/app_file.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var bodyParser = require('body-Parser');
+app.use(bodyParser.urlencoded({extended:false}));
 app.locals.pretty = true;
 app.set('views', './views');
 app.set('view engine', 'jade');
@@ -7,7 +10,15 @@ app.get('/topic/new', function(req, res) {
     res.render('new');
 })
 app.post('/topic', function(req, res) {
-    res.send('Hi, post');
+    var title = req.body.title;
+    var description = req.body.description;
+    fs.writeFile('data/' + title, description, function(err) {
+        if(err) {
+            res.status(500).send('Internet Server Error');
+        }
+        res.send('Success!');
+    });
+    res.send('Hi, post', + req.body.title);
 })
 app.listen(3000, function() {
     console.log('Connected 3000 Port!');
