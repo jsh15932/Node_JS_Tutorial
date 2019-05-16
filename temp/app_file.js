42085f80be90e781871ca47392a0f3d472f1d909
Hoon <jsh1400512@naver.com>
2019-05-17 00:58:01 +0900

    Create Project

--- temp/app_file.js
+++ temp/app_file.js
@@ -8,17 +8,53 @@ app.set('views', './views');
 app.set('view engine', 'jade');
 app.get('/topic/new', function(req, res) {
     res.render('new');
+});
+app.get(['/topic', '/topic/:id'], function(req, res) {
+    fs.readdir('data', function(err, files) {
+        if(err) {
+            console.log(err);
+            res.status(500).send('Internet Server Error');
+        }
+        var id = req.params.id;
+        if(id) {
+            fs.readFile('data/'+id, 'utf8', function(err, data) {
+                if(err) {
+                    console.log(err);
+                    res.status(500).send('Internet Server Error');
+                }
+                res.render('view', {title:files, title:id, description:data});
+            })
+        } else {
+            res.render('view', {topics:files});
+        }
+    })
+});
+app.get('/topic/:id', function(req, res) {
+    var id = req.params.id;
+    fs.readdir('data', function(err, files) {
+        if(err) {
+            console.log(err);
+            res.status(500).send('Internet Server Error');
+        }
+    fs.readFile('data/'+id, 'utf8', function(err, data) {
+        if(err) {
+            console.log(err);
+            res.status(500).send('Internet Server Error');
+        }
+        res.render('view', {title:files, title:id, description:data});
+     })
+    })
 })
 app.post('/topic', function(req, res) {
     var title = req.body.title;
     var description = req.body.description;
     fs.writeFile('data/' + title, description, function(err) {
         if(err) {
+            console.log(err);
             res.status(500).send('Internet Server Error');
         }
         res.send('Success!');
     });
-    res.send('Hi, post', + req.body.title);
 })
 app.listen(3000, function() {
     console.log('Connected 3000 Port!');
